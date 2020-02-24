301cbc4dca12373009b45db4e882d3e539c6215c
Tiago Dinis <tiagodinis33@gmail.com>
2020-02-24 21:09:18 +0000

    .

--- src/main/java/net/pomedge/events/ReactionEvents.java
+++ src/main/java/net/pomedge/events/ReactionEvents.java
@@ -8,6 +8,9 @@ import net.pomedge.main.Main;
 public class ReactionEvents extends ListenerAdapter {
 	@Override
 	public void onMessageReactionAdd(MessageReactionAddEvent event) {
+
+
+
 		Message msg = event.getChannel().getHistory().retrievePast(1).complete().get(0);
 		if(event.getUser() == event.getJDA().getSelfUser()) return;
 		if(event.getReactionEmote().getId() == "680732802244673598") {
